ea080fec1e44ffce49b99817ecc06ef3976183bd
Michael Mahon <michael.mahon@live.com>
2018-02-22 23:02:38 -0500

    Initial Commit

---
+++ WorkTimer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WorkTimer
+//
+//  Created by Michael Mahon on 2/22/18.
+//  Copyright © 2018 Michael Mahon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
